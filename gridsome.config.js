4927e474cc35ffce29ad6e74ec75107aa4395257
Van064886 <atadev13@gmail.com>
2022-05-18 14:44:37 +0200

    Conflicts resolved

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,7 +11,7 @@ const googleTM = require("./plugins/plugin.googleTM");
 module.exports = {
   siteName: "Rintio",
   siteDescription: "We involve for a better life",
-  siteUrl: `https://rintio.com`,
+  siteUrl: `https:rintio.com`,
   titleTemplate: "Rintio - %s",
   icon: "./src/assets/favicon.png",
   metadata: {
@@ -34,49 +34,46 @@ module.exports = {
     robot,
     sitemap,
     tailwind,
+    nodeExternals,
   ],
-
-  chainWebpack(config, { isServer }) {
-    config.module.rules.delete("svg");
-    config.module
-      .rule("svg")
-      .test(/\.svg$/)
-      .use("vue")
-      .loader("vue-loader")
-      .end()
-      .use("svg-to-vue-component")
-      .loader("svg-to-vue-component/loader");
-
-    if (isServer) {
-      config.externals(
-        nodeExternals({
-          allowlist: [
-            /\.css$/,
-            /\?vue&type=style/,
-            /vue-instantsearch/,
-            /instantsearch.js/,
-            /typeface-league-spartan/,
-          ],
-        })
-      );
-    }
-  },
-
-  configureWebpack(config) {
-    config.node = {
-      fs: 'empty',
-      child_process: 'empty',
-      net: 'empty',
-      dns: 'empty',
-      tls: 'empty',
-    };
-    return config;
-  },
-
   templates: {
     ContentfulPost: "/blog/article/:title",
-    //ContentfulCategory: "/category/:title",
-    //ContentfulTag: "/tag/:title",
   },
+   chainWebpack(config, { isServer }) {
+     config.module.rules.delete("svg");
+     config.module
+       .rule("svg")
+       .test(/\.svg$/)
+       .use("vue")
+       .loader("vue-loader")
+       .end()
+       .use("svg-to-vue-component")
+       .loader("svg-to-vue-component/loader");
+
+     if (isServer) {
+       config.externals(
+         nodeExternals({
+           allowlist: [
+             /\.css$/,
+             /\?vue&type=style/,
+             /vue-instantsearch/,
+             /instantsearch.js/,
+             /typeface-league-spartan/,
+           ],
+         })
+       );
+     }
+   },
+
+   configureWebpack(config) {
+     config.node = {
+       fs: 'empty',
+       child_process: 'empty',
+       net: 'empty',
+       dns: 'empty',
+       tls: 'empty',
+     };
+     return config;
+   },
 };
 
