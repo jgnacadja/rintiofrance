2b1580813ecce85c6441a9465322a883d6fb8f34
lionel okpeicha <lohnsonok@gmail.com>
2022-07-04 10:53:55 +0100

    fix: loading button alignment

--- src/pages/postuler.vue
+++ src/pages/postuler.vue
@@ -145,7 +145,7 @@
             </vue-hcaptcha>
           </div>
           <div class="submit">
-            <button type="submit" class="py-2 px-5 rounded bg-red-500 text-white">
+            <button type="submit" class="flex py-2 px-5 rounded bg-red-500 text-white">
               Envoyer
               <svg v-if="loading" class="animate-spin ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
                 fill="none" viewBox="0 0 24 24">
