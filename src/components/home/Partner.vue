b95c8a60baa9ed9788eb72a9030a437e31a4bf00
lohnsonok <lohnsonok@gmail.com>
2024-11-05 17:14:05 +0000

    fix: update partners

--- src/components/home/Partner.vue
+++ src/components/home/Partner.vue
@@ -7,7 +7,7 @@
       <div class="w-full text-center md:text-center lg:text-left">
         <div class="mx-2 md:mx-0">
           <h3 class="mb-3 text-sm text-primary font-semibold uppercase">
-            {{ title }}
+            {{ data.name }}
           </h3>
           <div class="-mt-3">
             <span
@@ -23,33 +23,25 @@
         </div>
       </div>
 
-      <div
-        v-for="partner in partners"
-        :key="partner.id"
-        class="w-full first:mt-14 mt-2"
-      >
-        <h3 class="px-4 mb-3 text-sm font-semibold uppercase text-primary">
-          {{ partner.name }}
-        </h3>
-
-        <div
-          class="flex-row justify-start items-center flex-wrap mx-auto flex lg:space-x-4"
-        >
-          <figure
-            v-for="image in partner.columns"
-            :key="image.id"
-            class="image is-16by9"
-          >
-            <g-image
-              :src="image.image.file.url"
-              class="w-40 h-auto"
-              :alt="image.title"
-              width="160"
-              height="96"
-            />
-          </figure>
+      <section class="py-24">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
+            <figure
+              v-for="partner in data.columns"
+              :key="partner.id"
+              class="bg-white image is-16by9 flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+            >
+              <g-image
+                :src="partner.image.file.url"
+                class="w-40 h-auto m-auto"
+                :alt="partner.image.title"
+                width="160"
+                height="96"
+              />
+            </figure>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   </LazyHydrate>
 </template>
@@ -68,14 +60,6 @@ export default {
   },
   components: {
     LazyHydrate,
-  },
-  computed: {
-    title() {
-      return this.data.name;
-    },
-    partners() {
-      return this.data.columns[0].columns;
-    },
   },
 };
 </script>
